e9317748ddb800cc10391559426b4aab3c90ca05
Прозоров Владислав <159719052+vladislavprozorov@users.noreply.github.com>
2025-10-21 01:45:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
+  base: '/cv/',
   plugins: [react()],
   optimizeDeps: {
     include: [
